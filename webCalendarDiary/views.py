b2100cb1093742a803aa3a098fabff77ea52180e
leeve1247 <multirkh@gmail.com>
2020-09-01 10:16:16 +0900

    일단 커밋

--- webCalendarDiary/views.py
+++ webCalendarDiary/views.py
@@ -17,11 +17,9 @@ class HomeView(ScheduleLV):
 	# 강사 추가 부분
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
-		year = self.request.GET.get("year", "2020")	
-		month = self.request.GET.get("month", "08")
-		schedules = Schedule.objects.filter(schedule_date__year=year,
-								schedule_date__month=month) 
-
+		year = self.request.GET.get("year", "2020")
+		month = self.request.GET.get("month", "09")
+		schedules = Schedule.objects.filter(user_id_fk=self.request.user.id)
 		context['schedule_list'] = schedules
 		return context
 
