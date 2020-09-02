5db9fdffc7c41c6832ee7dd8889131ab3a706b85
leeve1247 <multirkh@gmail.com>
2020-09-02 16:09:00 +0900

    중간저장

--- mypage/views.py
+++ mypage/views.py
@@ -28,26 +28,12 @@ class mypage_view(LoginRequiredMixin, DetailView):
 class MypageFormed(LoginRequiredMixin,TemplateView):
 	template_name = 'mypage_done.html'
 
-class MyPageCV(CreateView):
-	template_name = 'mypage_form.html'
-	model = UserInfo
-	fields = ['phoneNumber', 'address', 'nickname']
-	success_url = reverse_lazy('mypage_done')  # 데이터 입력이 원활히 전송이 됐을 경우 mypage_index로 회귀
-
-	def form_valid(self, form):  # 유효성 검사
-		form.instance.owner = self.request.user  # user는 field데이터에서 구성되어있지 않으므로 사용자 권한 전달
-		try: #기존 데이터가 있으면 삭제
-			userinfo = UserInfo.objects.get(owner_id=self.request.user.id)
-			userinfo.delete()
-		except:
-			pass
-		return super().form_valid(form)  # 데이터 베이스로 넘기는 과정
 
 class MyPageUV(LoginRequiredMixin,UpdateView):
 	template_name = 'mypage_form.html'
 	model = UserInfo
 	fields = ['phoneNumber', 'address', 'nickname']
-	success_url = reverse_lazy('mypage_done')  # 데이터 입력이 원활히 전송이 됐을 경우 mypage_index로 회귀
+	success_url = reverse_lazy('home.html')  # 데이터 입력이 원활히 전송이 됐을 경우 mypage_index로 회귀
 
 	def get_object(self, queryset=None):
 		return UserInfo.objects.get(owner=self.request.user)
