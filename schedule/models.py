4f1e799622dd0ba1c042a94e97467d73322229da
junnn_ng <ljw2247@gmail.com>
2020-10-11 18:16:32 +0900

    twilio문자서비스추가_유저에따른번호변경필요

--- schedule/models.py
+++ schedule/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+from twilio.rest import Client
 
 # Create your models here.
 
@@ -18,3 +19,17 @@ class Schedule(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        account_sid = 'AC598feaeef0ecd3f0508c56e8944c01e4'
+        auth_token = 'e9d9591701607c8308eafdfb2d647076'
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+            body=f'Hello from Calda. Your schedule is {self.title} on {self.schedule_date}',
+            from_='+13342922554',
+            to='+821093690964'
+        )
+
+        print(message.sid)
+        return super().save(*args, **kwargs)
